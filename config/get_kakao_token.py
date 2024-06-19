60c3d4874af474d861b839cbf3cbdeeb21293c88
Kdreamtomaster <kang555668@gmail.com>
2024-06-19 10:28:34 +0900

    kakao

--- config/get_kakao_token.py
+++ config/get_kakao_token.py
@@ -1,9 +1,9 @@
 import requests
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = '$$$$$'
+client_id = '{client_id}'
 redirect_uri = 'https://exmaple.com/oauth'
-authorize_code = '$$$$$'
+authorize_code = '{authorized_code}'
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
