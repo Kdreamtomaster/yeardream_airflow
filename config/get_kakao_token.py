40cc51445937ff83d0ff2cacf57482d833a97dca
ShinWoo <166012861+Kdreamtomaster@users.noreply.github.com>
2024-06-19 10:15:37 +0900

    Update get_kakao_token.py

--- config/get_kakao_token.py
+++ config/get_kakao_token.py
@@ -1,9 +1,9 @@
 import requests
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = '6c64535173392f61f5cc7f35089be7b2'
+client_id = '$$$$$'
 redirect_uri = 'https://exmaple.com/oauth'
-authorize_code = 'gEX3Oo-W6BJUIO5UQxbJjMvzT-kkORlhqY6uMAEpt1VRgF53K_PQtwAAAAQKKcjYAAABkC35tja2xj-RG-1vuA'
+authorize_code = '$$$$$'
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
